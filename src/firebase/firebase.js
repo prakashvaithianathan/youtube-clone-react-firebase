cb5954a657d1c87150d811bc689a203e12200c05
prakashsparkz <indupraka18@gmail.com>
2021-07-30 22:28:43 +0530

    initial commit

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,17 +3,9 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/storage'
 import 'firebase/firestore'
+import {firebaseConfig} from './config'
 
 
-
-const firebaseConfig = {
-    apiKey: "AIzaSyAPufljn2w57bCRxf7ln9ga5-PoX0yYW_8",
-    authDomain: "utube-sparkz.firebaseapp.com",
-    projectId: "utube-sparkz",
-    storageBucket: "utube-sparkz.appspot.com",
-    messagingSenderId: "486932816570",
-    appId: "1:486932816570:web:c54bcce60843744f7d5883"
-  };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig)
 
